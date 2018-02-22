373849c07133bbc1b3a58872895ef6449da16f78
Joe <joe831216@gmail.com>
2018-02-22 12:37:52 +0800

    Now can get application name in ReflectionDemo.java

--- feign-consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ feign-consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -3,11 +3,17 @@ package com.example.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 // 啟動Feign功能
 @EnableFeignClients
 @SpringBootApplication
 public class ConsumerApplication {
+
+	@Bean
+	public ReflectionDemo reflectionDemo() {
+		return new ReflectionDemo();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
--- feign-consumer/src/main/java/com/example/consumer/ReflectionDemo.java
+++ feign-consumer/src/main/java/com/example/consumer/ReflectionDemo.java
@@ -1,27 +1,45 @@
+package com.example.consumer;
+
 import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Set;
 
-public class ReflectionDemo {
-    public static void main(String args[]) {
+@Component
+public class ReflectionDemo implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionDemo.class);
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    public void run(String... args) throws Exception {
         // 取得套件下所有類別
         Reflections reflections = new Reflections("", new SubTypesScanner(false));
         Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
+        // 取得服務名稱
+        logger.info("Service name: " + env.getProperty("spring.application.name"));
         for (Class c : classes) {
             // 若該類別為介面且@FeignClient不為空，則印出詳細資訊
             if (c.isInterface() && c.getAnnotation(FeignClient.class) != null) {
-                System.out.println("Value of " + c.getName());
+                logger.info("    Value of: " + c.getName());
                 Annotation feignClientAnnotation = c.getAnnotation(FeignClient.class);
                 Method[] methods = feignClientAnnotation.annotationType().getDeclaredMethods();
                 for (Method method : methods) {
                     try {
                         Object value = method.invoke(feignClientAnnotation, (Object[])null);
-                        System.out.println("    " + method.getName() + " : " + value);
+                        logger.info("        " + method.getName() + ": " + value);
                     } catch (IllegalAccessException e) {
                         System.out.println("IllegalAccessException");
                     } catch (InvocationTargetException e) {
